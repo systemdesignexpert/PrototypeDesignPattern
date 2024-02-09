d320bebb3b2170e7eff3d780919917eda1f504f5
Aditya Prakash <AdityaPrakash@MacBook-Pro-M1.local>
2024-02-09 19:10:30 +0530

    adding changes

---
+++ PrototypeDesignPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿class GraphicsApp
+{
+    private static void Main(string[] args)
+    {
+        IShapePrototype originalCircle = new Circle(10);
+        IShapePrototype originalRectangle = new Rectangle(20, 15);
+        IShapePrototype originalTriangle = new Triangle(3, 4, 5);
+
+        // Cloning prototypes to create new instances
+        Circle cloned = new Circle(10);
+        
+        IShapePrototype clonedCircle = originalCircle.Clone();
+        IShapePrototype clonedRectangle = originalRectangle.Clone();
+        originalTriangle.Draw();
+        IShapePrototype clonedTriangle = originalTriangle.Clone();
+
+
+
+        originalCircle.Draw(); // Original circle
+        clonedCircle.Draw();   // Cloned circle
+        clonedTriangle.Draw();
+
+        originalRectangle.Draw(); // Original rectangle
+        clonedRectangle.Draw();   // Cloned rectangle
+    }
+}
